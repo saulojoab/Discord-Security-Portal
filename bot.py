63912eccde8e57fdda6fa5f5ec1fcac82fd9bcd3
Saulo <saulojoabps@gmail.com>
2019-06-27 01:00:06 -0300

    ban apparently working

--- bot.py
+++ bot.py
@@ -35,9 +35,11 @@ class MyClient(discord.Client):
             words = messageStr.split(" ")
 
             # Infraction verification
-            if (words.__len__() > 3 and words[0] == "infraction"):
+            if (words.__len__() > 2 and words[0] == "infraction"):
                 await self.Infraction(words, message);
-            if (words[0] == "say" and words.__len__() > 1):
+            elif (words.__len__() > 2 and words[0] == "ban"):
+                await self.Ban(words, message);
+            elif (words[0] == "say" and words.__len__() > 1):
                 await self.Say(words, message);
             else:
                 await message.channel.send("Im sorry? I have no idea what you said... :sob:");
@@ -107,6 +109,51 @@ class MyClient(discord.Client):
         await message.channel.send(":cop: Hey, " + str(message.author) + "! Your report has been registered. Details: ");
         await message.channel.send("```- Infraction: " + infraction + "\n- "
                                    "User: " + commandList[1] + "\n- Action Taken: " + commandList[0] + "```");
+
+    async def Ban(self, commandList, message):
+        """
+        This method handles the ban action.
+        :param commandList:
+        :param message:
+        :return:
+        """
+        # If someone tries to register an infraction against our bot.
+        if (commandList[1] == "<@593308038476201999>"):
+            await message.channel.send("Ha ha ha. You're so silly :stuck_out_tongue:");
+            return;
+
+        # Loggin info.
+        print(str(message.author) + " is banning someone! Details:");
+
+        # Forming the infraction description.
+        infraction = "";
+        for idx, i in enumerate(commandList):
+            if (idx >= 2):
+                infraction += i + " ";
+            if (idx == commandList.__len__()):
+                infraction += i;
+
+        # Logging.
+        print("- Infraction: " + infraction);
+        print("- User: " + commandList[1]);
+        print("- ActionTaken: " + commandList[0]);
+
+        string = commandList[1];
+        for i in ['<', '@', '>']:
+            string = string.replace(i, "");
+        await message.author.guild.ban(discord.Object(id=string));
+
+        # Loading...
+        await message.channel.send("THE BAN HAMMER HAS BEEN APPLIED! :cowboy: :cop: now lemme make a report for you...");
+        res = apiHandler.addInfraction(commandList[1], infraction, commandList[0]);
+        print(res);
+
+        # Logging info to user.
+        await message.channel.send(
+            ":cop: Hey, " + str(message.author) + "! Your report has been registered. Details: ");
+        await message.channel.send("```- Infraction: " + infraction + "\n- "
+                                                                      "User: " + commandList[1] + "\n- Action Taken: " +
+                                   commandList[0] + "```");
 
 client = MyClient()
 client.run(str(cred["bottoken"]))
